5a97a75dec4b95c290eb5e2baec7268776c57e60
ee17856 <ee17856@essex.ac.uk>
2024-09-06 15:57:59 +0100

    Added a cache service to store fetched data for performance optimisation

--- src/services/cacheService.ts
+++ src/services/cacheService.ts
@@ -1,1 +1,18 @@
 //Caches API results
+
+const cache: { [key: string]: { data: any; expiry: number } } = {};
+
+const CACHE_DURATION = 60 * 1000; // cache 1 minute
+
+export const setCache = (key: string, data: any) => {
+  const expiry = Date.now() + CACHE_DURATION;
+  cache[key] = { data, expiry };
+};
+
+export const getCache = (key: string): any | null => {
+  const cachedItem = cache[key];
+  if (cachedItem && cachedItem.expiry > Date.now()) {
+    return cachedItem.data;
+  }
+  return null;
+};
